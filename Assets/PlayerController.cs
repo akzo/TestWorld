41ef38af9ad2a1c93b198de2d7dc951a15662b2f
akzo <anarkazimov98@hotmail.com>
2018-09-26 20:29:09 -0700

    Adding a test world
    Hey guys I have added the test world please don't say Hey guys in the formal git commits

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Camera myCamera;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0, 0, 2);
+            myCamera.transform.Translate(0, 0, 2);
+        }
+	}
+}
